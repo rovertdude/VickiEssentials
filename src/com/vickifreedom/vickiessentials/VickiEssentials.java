32399bfd5a6587acd579b23e97d960955c5a295f
rovertdude <tsannoyingorange@gmail.com>
2016-07-01 18:44:56 -0500

    Uploaded all files
    just to test rn

---
+++ src/com/vickifreedom/vickiessentials/VickiEssentials.java
@@ -0,0 +1,15 @@
+package com.vickifreedom.vickiessentials;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VickiEssentials extends JavaPlugin {
+
+// Fired when plugin is first enabled
+@Override
+public void onEnable() {
+}
+// Fired when plugin is disabled
+@Override
+public void onDisable() {
+
+}
+}
